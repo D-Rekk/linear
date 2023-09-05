5c4517c453de12a1175ff30e74bfb61d0e52b101
D-Rekk <andrea_derek@hotmail.it>
2023-09-05 23:49:59 +0200

    set styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,39 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      sans: `"SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif`,
     },
+    fontSize:{
+      md: "1.6rem",
+      lg: ["2.2rem", "1,3"],
+      "5xl": ["8rem", "1"] ,
+    },
+    colors: {
+      white: "#fff",
+      background: "#000212",
+    },
+    spacing:{
+      0: "0",
+      1: "0.4rem",
+      2: "0.8rem",
+      3: "1.2rem",
+      4: "1.6rem",
+      5: "2rem",
+      6: "2.4rem",
+      7: "2.8rem",
+      8: "3.2rem",
+      9: "3.6rem",
+      10: "4rem",
+      11: "4.4rem",
+      12: "4.8rem"
+    }
   },
   plugins: [],
-}
+};
 export default config
