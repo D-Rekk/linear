303545719697fd5a5eaedf7f243975e5b74bd64a
D-Rekk <andrea_derek@hotmail.it>
2023-09-27 19:37:14 +0200

    create 'medium' custom class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
   content: [
@@ -10,16 +11,23 @@ const config: Config = {
     fontFamily: {
       sans: `"SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif`,
     },
-    fontSize:{
+    fontSize: {
       md: "1.6rem",
       lg: ["2.2rem", "1.3"],
-      "5xl": ["8rem", "1"] ,
+      "5xl": ["8rem", "1"],
     },
     colors: {
       white: "#fff",
       background: "#000212",
+      linkHover: "#b4bcd099",
     },
-    spacing:{
+    customUtilities: {
+      '.medium': {
+        transition: 'color 0.25s linear'
+      },
+
+    },
+    spacing: {
       0: "0",
       1: "0.4rem",
       2: "0.8rem",
@@ -32,9 +40,15 @@ const config: Config = {
       9: "3.6rem",
       10: "4rem",
       11: "4.4rem",
-      12: "4.8rem"
-    }
+      12: "4.8rem",
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addUtilities, addComponents, theme }) {
+      addBase(theme('customBase', {}))
+      addComponents(theme('customComponents', {}))
+      addUtilities(theme('customUtilities', {}))
+    }),
+  ],
 };
 export default config
