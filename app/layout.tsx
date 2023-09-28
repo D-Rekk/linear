73183400bdf21277e82dd822ac5ced50f928be29
D-Rekk <andrea_derek@hotmail.it>
2023-09-28 10:04:49 +0200

    reorder of classes and use of tailwing merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>{children}</main>
+        <main className='mt-12'>{children}</main>
         <Footer />
       </body>
     </html>
--- components/container.tsx
+++ components/container.tsx
@@ -1,8 +1,12 @@
 import { Children } from "@/app/layout"
+import { twMerge } from "tailwind-merge"
 
-export const Container = ({children}: Children) => {
+type T_Container = Children & {
+  className?: string
+}
+export const Container = ({children, className} : T_Container) => {
   return (
-    <div className="max-w-[120rem] mx-auto">
+    <div className={twMerge(className, "max-w-[120rem] mx-auto")}>
       {children}
     </div>
   )
