06891b0172d407bacd300bf63ab48bcc5745d7bb
D-Rekk <andrea_derek@hotmail.it>
2023-10-03 23:42:49 +0200

    improve Footer usability

--- components/footer.tsx
+++ components/footer.tsx
@@ -24,26 +24,25 @@ const list = [
 export default function Footer() {
   return (
     <footer className="border-t border-white/5 text-md lg:text-sm ">
-      <Container className="flex flex-col lg:flex-row py-13 w-full ">
+      <Container className="flex flex-row flex-wrap py-13 w-full ">
         <section className="flex flex-row justify-between mr-auto lg:flex-col max-lg:w-full ">
           <Link href="/" className="flex items-center text-gray/60 hover:text-hover transition-color-hover">
             <Logo className="w-[1.8rem] h-[1.8rem] mr-2" />Linear - Designed Worldwide
           </Link>
-          <div className="flex">
-            <Link href={"#"} className="w-[1.8rem] h-[1.8rem] mr-2 p-3">
+          <div className="flex gap-6">
+            <Link href={"#"}>
               <TwitterIcon className="w-6 lg:w-[1.8rem] fill-gray/60 hover:fill-hover transition-color-hover" />
             </Link>
-            <Link href={"#"} className="w-[1.8rem] h-[1.8rem] mr-2">
+            <Link href={"#"}>
               <GithubIcon className="w-6 lg:w-[1.8rem] fill-gray/60 hover:fill-hover transition-color-hover"/>
             </Link>
-            <Link href={"#"} className="w-[1.8rem] h-[1.8rem] mr-2">
+            <Link href={"#"}>
               <SlackIcon className="w-6 lg:w-[1.8rem] fill-gray/60 hover:fill-hover transition-color-hover" />
             </Link>
           </div>
         </section>
-        <div className="flex flex-row lg:gap-13 flex-wrap">
           {list.map(({ title, contents }, i) => (
-            <section key={title + '' + i} className="min-w-[50%] lg:min-w-sections max-lg:mt-10 flex-1">
+            <section key={title + '' + i} className="min-w-[50%] lg:min-w-sections max-lg:mt-10 max-lg:flex-1">
               <h3 className="font-semibold mb-4 text-gray">{title}</h3>
               <ul>
                 {contents.map((content) => (
@@ -58,7 +57,6 @@ export default function Footer() {
           )
           )
           }
-        </div>
       </Container>
     </footer>
   )
