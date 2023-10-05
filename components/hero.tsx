eb213aecfa9d7e968c4d3e3d8ee28195b036fc1e
D-Rekk <andrea_derek@hotmail.it>
2023-10-05 10:04:03 +0200

    adjust font sizes

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ import { twMerge } from "tailwind-merge"
 export const HeroTitle = ({ children, className }: ClassChildren) => {
   return (
 
-    <h1 className={twMerge(className,"text-5xl my-6")}>{children}</h1>
+    <h1 className={twMerge(className,"text-7xl my-6 font-medium")}>{children}</h1>
   )
 }
 export const HeroSub = ({ children, className }: ClassChildren) => {
