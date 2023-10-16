703bb502ef68b98e4f8960c07cb34fec07b8fa36
D-Rekk <andrea_derek@hotmail.it>
2023-10-16 15:23:04 +0200

    add Arrow component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { Button, IconWrapper } from "@/components/button";
 import { Container } from "@/components/container"
 import { Hero, HeroSub, HeroTitle } from "@/components/hero"
-
+import { LeftArrow } from "@/components/icons/arrow";
 export default function Homepage() {
   return (
     <div>
       <Container>
         <Hero className="mt-[6.4rem]">
+          <Button href="#" size={"small"} variant={"secondary"}>
+            Announcing our $35M Series B
+            <IconWrapper>
+              <LeftArrow/>
+            </IconWrapper>
+            </Button>
           <HeroTitle className="text-balance">Linear is a better way<br className="hidden lg:block"/> to build products</HeroTitle>
           <HeroSub className="text-balance text-gray"> Meet the new standard for modern software development.<br className="hidden md:block"/> Streamline issues, sprints, and product roadmaps.
           </HeroSub>
