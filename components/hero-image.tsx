1277c5752ad15471d4055110387738c27c12dc62
D-Rekk <andrea_derek@hotmail.it>
2023-10-23 15:37:00 +0200

    add glowing wrapper to the HeroImage component

--- components/hero-image.tsx
+++ components/hero-image.tsx
@@ -9,21 +9,35 @@ export function HeroImage () {
   let isApplied = useRef<boolean>(false)
 
   useEffect(() => {
-    if(isIntersecting && !isApplied.current) { // Component enters viewport by 30%
+    if (isIntersecting && !isApplied.current) { // Component enters viewport by 30%
       console.log("It's intersecting we add style");
-      ref.current?.classList.add('hero-image-animation');
-      isApplied.current = true
-    }
-    else if (!isStrictInterecting && isApplied.current){ //Left completely
+      ref.current?.classList.add("hero-image-animation");
+      ref.current?.previousElementSibling?.classList.add("glow");
+      ref.current?.firstElementChild?.classList.add("toggle-opacity");
+      isApplied.current = true;
+    } else if (!isStrictInterecting && isApplied.current) { //Left completely
       console.log("We removing Style brr");
-      ref.current?.classList.remove('hero-image-animation');
-      isApplied.current = false
+      ref.current?.classList.remove("hero-image-animation");
+      ref.current?.previousElementSibling?.classList.remove("glow");
+      ref.current?.firstElementChild?.classList.remove("toggle-opacity");
+      isApplied.current = false;
     }
   }, [isIntersecting, isStrictInterecting])
 
   return (
-    <div ref={ref} className="hero-image-start bg-hero-gradient bg-white/[0.01] mt-[12.8rem] rounded-lg animation-duration-[1.4s] animation-delay-[400ms]">
-      <img className="mt-[12.8rem] opacity-0" src="/img/hero.webp" alt={"hero-img"} />
+    <div className="hero [perspective:200rem] mt-[12.8rem]">
+      <div className="glow-base" />
+      <div
+        ref={ref}
+        className="hero-image-wrapper bg-hero-gradient bg-white/[0.01] rounded-lg border border-white/5
+        animation-duration-[1.4s] animation-delay-[400ms]"
+      >
+        <img
+          className="opacity-0 hero-image"
+          src="/img/hero.webp"
+          alt={"hero-img"}
+        />
+      </div>
     </div>
   );
 }
