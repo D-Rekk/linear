14cf1cfacfb8a099079b26adc93eb197172af4f2
D-Rekk <andrea_derek@hotmail.it>
2023-10-24 10:51:19 +0200

    include silhouette and cleanup HeroImage

--- components/hero-image.tsx
+++ components/hero-image.tsx
@@ -1,44 +1,58 @@
-"use client"
-import { useEffect, useRef } from "react"
-import useIntersectionObserver from "@react-hook/intersection-observer"
+"use client";
+import { useEffect, useRef, useState } from "react";
+import useIntersectionObserver from "@react-hook/intersection-observer";
+import { ClassChildren } from "@/app/types";
+import clsx from "clsx";
 
-export function HeroImage () {
-  const ref = useRef<HTMLDivElement>(null)
-  const {isIntersecting} = useIntersectionObserver(ref, {threshold : 0.3})
-  const {isIntersecting: isStrictInterecting} = useIntersectionObserver(ref)
-  let isApplied = useRef<boolean>(false)
+export function HeroImage() {
+  const ref = useRef<HTMLDivElement>(null);
+  const { isIntersecting } = useIntersectionObserver(ref, { threshold: 0.3 });
+  const { isIntersecting: isStrictInterecting } = useIntersectionObserver(ref);
+  let [isVisible, setIsVisible] = useState<boolean>(false);
 
   useEffect(() => {
-    if (isIntersecting && !isApplied.current) { // Component enters viewport by 30%
+    if (isIntersecting && !isVisible) {
+      // Component enters viewport by 30%
       console.log("It's intersecting we add style");
-      ref.current?.classList.add("hero-image-animation");
-      ref.current?.previousElementSibling?.classList.add("glow");
-      ref.current?.firstElementChild?.classList.add("toggle-opacity");
-      isApplied.current = true;
-    } else if (!isStrictInterecting && isApplied.current) { //Left completely
+      setIsVisible((isVisible) => true)
+    } else if (!isStrictInterecting && isVisible) {
+      //Left completely
       console.log("We removing Style brr");
-      ref.current?.classList.remove("hero-image-animation");
-      ref.current?.previousElementSibling?.classList.remove("glow");
-      ref.current?.firstElementChild?.classList.remove("toggle-opacity");
-      isApplied.current = false;
+      setIsVisible((isVisible) => false);
     }
-  }, [isIntersecting, isStrictInterecting])
+  }, [isIntersecting, isStrictInterecting]);
 
   return (
-    <div className="hero [perspective:200rem] mt-[12.8rem]">
-      <div className="glow-base" />
+    <section className="hero [perspective:200rem] mt-[12.8rem]">
+      <div className={clsx("glow-base",
+      isVisible ? "glow" : "")} />
       <div
         ref={ref}
-        className="hero-image-wrapper bg-hero-gradient bg-white/[0.01] rounded-lg border border-white/5
-        animation-duration-[1.4s] animation-delay-[400ms]"
+        className={clsx(
+          `hero-image-wrapper bg-hero-gradient bg-white/[0.01] grid
+        rounded-lg border border-white/5 animation-duration-[1.4s] animation-delay-[400ms]`,
+          isVisible ? "hero-image-animation" : ""
+        )}
       >
+        <ImageOutLine className={clsx("silhouette-base w-full h-full", isVisible ? "silhouette" : "")} />
         <img
-          className="opacity-0 hero-image"
+          className={clsx(
+            "opacity-0 hero-image",
+            isVisible ? "toggle-opacity" : ""
+          )}
           src="/img/hero.webp"
           alt={"hero-img"}
         />
       </div>
-    </div>
+    </section>
   );
 }
-
+const ImageOutLine = ({ className }: ClassChildren) => (
+  <svg width="100%" fill="none" className={className}>
+    <path pathLength="1" d="M1500 72L220 72"></path>
+    <path pathLength="1" d="M1500 128L220 128"></path>
+    <path pathLength="1" d="M1500 189L220 189"></path>
+    <path pathLength="1" d="M220 777L220 1"></path>
+    <path pathLength="1" d="M538 777L538 128"></path>
+  </svg>
+);
